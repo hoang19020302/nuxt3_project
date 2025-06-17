5d225f5df272880002eaf711b596028ac6f51cdc
hoang19020302 <tranichhoang2001@gmail.com>
2025-06-18 01:56:21 +0700

    setup ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,5 @@
 set -e
 echo "🚀 Nuxt starting..."
 
-# Nếu là file mới copy thành công
-if [ -f "/tmp/env_config.js" ]; then
-    cp /tmp/env_config.js /usr/share/nginx/html/env_config.js
-else
-    echo "❌ Không tìm thấy /tmp/env_config.js hoặc không phải là file."
-fi
-
 echo "🚀 Khởi động Nuxt App..."
 exec nginx -g 'daemon off;'
